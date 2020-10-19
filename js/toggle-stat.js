ee1615827c1f729ed017b48b47b256f20be0f3b5
M_2004586 <m_2004586@ambus130871.fios-router.home>
2020-10-19 16:27:26 -0400

    update variable naming

--- js/toggle-stat.js
+++ js/toggle-stat.js
@@ -1,23 +1,23 @@
 
 
-function shuffle_stat(stat1, stat2) { 
-    var stat1_id = document.getElementById(stat1);
-    var stat2_id = document.getElementById(stat2);
+function suffleStat(stat1, stat2) { 
+    var stat_1 = document.getElementById(stat1);
+    var stat_2 = document.getElementById(stat2);
     
-    var arr = [stat1_id, stat2_id]
-    var sorted_arr = arr.sort(() => Math.random() - 0.5);
-    return sorted_arr;
+    var arr = [stat_1, stat_2]
+    var sortedArr = arr.sort(() => Math.random() - 0.5);
+    return sortedArr;
 } 
 
-function display_stat(shuffled_stat_arr){
-    shuffled_stat_arr[0].style.display = "block";
-    shuffled_stat_arr[1].style.display = "none";
+function displayStat(shuffledStatArr){
+    shuffledStatArr[0].style.display = "block";
+    shuffledStatArr[1].style.display = "none";
 }
 
 window.onload = function () {
-    var stat_1_4 = shuffle_stat("stat1", "stat4-sprint2");
-    var stat_2_5 = shuffle_stat("stat2", "stat5-sprint2")
+    var stat_1_4 = suffleStat("stat1", "stat4-sprint2");
+    var stat_2_5 = suffleStat("stat2", "stat5-sprint2")
 
-    display_stat(stat_1_4)
-    display_stat(stat_2_5)
+    displayStat(stat_1_4)
+    displayStat(stat_2_5)
 }
